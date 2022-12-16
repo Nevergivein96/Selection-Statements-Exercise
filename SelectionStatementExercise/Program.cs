2b1692f3f97b7fe7044c53373e3242f9e6849387
Spencer Hansen <spencerhansen@gmail.com>
2022-12-15 17:49:33 -0700

    completed the guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            int userInput;
+            var counter = 0;
+
+            do
+            {
+                Console.WriteLine("guess a number between 1 and 1000");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput > favNumber)
+                {
+                    Console.WriteLine("that guess is too high");
+                }
+
+                else if (userInput < favNumber)
+                {
+                    Console.WriteLine("that guess is too low");
+                }
+
+                else
+                {
+                    Console.WriteLine("congradulations!! you guessed correctly");
+                }
+                counter++;
+                Console.WriteLine($"you have guessed this many times {counter}");
+            }
+            while (userInput != favNumber);   
+
         }
     }
 }
